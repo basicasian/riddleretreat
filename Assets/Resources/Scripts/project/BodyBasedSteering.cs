c046f66e099debdf81745c895c7e279fd90d5ec9
basicasian <nana.zhang.00@gmail.com>
2023-12-01 23:26:53 +0100

    added body based steering

---
+++ Assets/Resources/Scripts/project/BodyBasedSteering.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.XR.CoreUtils;
+
+public class BodyBasedSteering : MonoBehaviour
+{
+    public InputActionReference steeringReference = null;
+    public Camera mainCamera = null;
+    public XROrigin xrOrigin = null;
+
+    public float speed = 0;
+
+    private bool isActive = true;
+
+    void Update()
+    {
+        if (steeringReference.action.IsPressed() &&  isActive)
+        {      
+            Steering();
+        }
+    }
+
+    public void Steering()
+    {
+        Vector3 deltaSteering = (Vector3.Scale(mainCamera.transform.forward, new Vector3(1.0f, 0.0f, 1.0f)));
+        xrOrigin.transform.position += deltaSteering * speed * Time.deltaTime;
+    }
+
+    public void SetActive(bool value)
+    {
+        isActive = value;
+    }
+
+    public bool getActive()
+    {
+        return isActive;
+    }
+}
