af186ba582f2fae5044cfaae851f9ac233b16447
basicasian <nana.zhang.00@gmail.com>
2023-12-11 17:49:33 +0100

    added helper postet

--- Assets/Scripts/project/button wall/DirectTouchController.cs
+++ Assets/Scripts/project/button wall/DirectTouchController.cs
@@ -9,15 +9,10 @@ public class DirectTouchController : MonoBehaviour
     public GameObject wallGameObject;
     private ButtonController buttonControllerScript;
 
-    public GameObject startGameLogic;
-    private StartGameLogic startGameLogicScript;
-
-
     // Start is called before the first frame update
     void Start()
     {
         buttonControllerScript = wallGameObject.GetComponent<ButtonController>();
-        startGameLogicScript = startGameLogic.GetComponent<StartGameLogic>();   
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,11 +23,6 @@ public class DirectTouchController : MonoBehaviour
             buttonControllerScript.isTouched = true;
             GetComponent<Image>().color = new Color(0.8f, 0.8f, 0.8f, 1);
         }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-
     }
 
     private void OnTriggerExit(Collider other)
